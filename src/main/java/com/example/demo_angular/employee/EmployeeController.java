ab473cfbbc7e59789fb8ddcabccec09285e92d1e
qkb2 <53836634+qkb2@users.noreply.github.com>
2023-08-08 18:33:39 +0200

    Added employee service.

---
+++ src/main/java/com/example/demo_angular/employee/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.example.demo_angular.employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        Assert.notNull(employeeRepository, "employeeRepository must not be null!");
+        this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewEmployee(
+            @RequestParam String firstName,
+            @RequestParam String lastName
+    ) {
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employeeRepository.save(employee);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Employee getEmployee(@PathVariable Integer id) {
+        return employeeRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public @ResponseBody String deleteEmployee(@PathVariable Integer id) {
+        employeeRepository.deleteById(id);
+        return "Deleted";
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody String updateEmployee(
+            @PathVariable Integer id,
+            @RequestParam String firstName,
+            @RequestParam String lastName
+    ) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(RuntimeException::new);
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employeeRepository.save(employee);
+        return "Updated";
+    }
+}
